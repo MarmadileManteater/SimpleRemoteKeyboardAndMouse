cc66fd6d70e74bb84304ec67545a266ee11f2a11
Emma <MarmadileManteater@proton.me>
2023-06-19 07:49:01 -0400

    js: assign only modified fields to `state` object instead of recreating object

--- static/script.js
+++ static/script.js
@@ -35,7 +35,7 @@
     let touchpad = document.querySelector('.touch')
     touchpad.addEventListener('touchstart', (e) => {
         e.preventDefault()
-        state = { x: e.touches[0].clientX, y: e.touches[0].clientY, isTouching: true, isScrolling: e.touches.length > 1, lastTimeSent: state.lastTimeSent }
+        Object.assign(state, { x: e.touches[0].clientX, y: e.touches[0].clientY, isTouching: true, isScrolling: e.touches.length > 1 })
     })
     touchpad.addEventListener('touchmove', (e) => {
         // if the user is currently touching the screen and the last time trackpad data was sent is greater than 150ms ago,
@@ -47,12 +47,12 @@
             let dx = parseInt(state.x - touches[0][0])
             let dy = parseInt(state.y - touches[0][1])
             // set the d object to the current client x and y values
-            state = { x: touches[0][0], y: touches[0][1], isScrolling: touches.length == 2?calculateDistance.apply(null, touches) < 100:false, isTouching: state.isTouching, lastTimeSent: new Date().getTime() }
+            Object.assign(state, { x: touches[0][0], y: touches[0][1], isScrolling: touches.length == 2?calculateDistance.apply(null, touches) < 100:false, lastTimeSent: new Date().getTime() })
             fetch(`/mouse${state.isScrolling?'scroll':isDragToggled?'drag':'move'}/${(- dx * 2)}/${(- dy * 2)}`)
         }
     })
     touchpad.addEventListener('touchend', () => {
         // clear out the position  object and the isTouching variable
-        state = { x: 0, y: 0, isTouching: false, isScrolling: false, lastTimeSent: state.lastTimeSent }
+        Object.assign(state, { x: 0, y: 0, isTouching: false, isScrolling: false })
     })
 })()
